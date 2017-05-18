eb9a464eb17a30f0bc633f2f5e1a4581c821676a
Benoit Averty <benoit.averty@gmail.com>
2017-05-18 15:32:45 +0200

    refactor(searchCycles): replace string literal by action

--- src/redux/cycles/searchCycles.js
+++ src/redux/cycles/searchCycles.js
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs'
 import { ifElse, prop, propEq, isEmpty, not, isNil, pipe, trim, F } from 'ramda'
 
+import { changeSearch } from '../actions'
 import { codepointSearchRequest } from './graphqlRequests'
 
 // Return true if a string is neither undefined nor empty
@@ -9,7 +10,7 @@ const isNotBlank = ifElse(isNil, F, pipe(trim, isEmpty, not))
 export default function search({ Action, Http, Time }) {
 
   const codepointRequest$ = Action
-    .filter(propEq('type', 'CHANGE_SEARCH'))
+    .filter(propEq('type', changeSearch.toString()))
     .let(Time.debounce(250))
     .map(prop('payload'))
     .distinctUntilChanged()
