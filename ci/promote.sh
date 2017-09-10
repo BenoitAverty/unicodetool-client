3a0bddf67a836c2cecdafe3a837db5d277628067
Benoit Averty <benoit.averty@gmail.com>
2017-09-10 20:10:43 +0200

    chore(ci): Disabling promotion for pull requests to master

--- ci/promote.sh
+++ ci/promote.sh
@@ -4,7 +4,8 @@ set -e
 
 promoted_url=$1
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_EVENT_TYPE" == "push" ]; then
+    >&2 echo "Promoting $promoted_url to production (unicodetool-client.now.sh)"
     >&2 now alias $promoted_url unicodetool-client.now.sh -t $NOW_TOKEN
 else
     >&2 echo "Not on branch master. Skipping promotion."
