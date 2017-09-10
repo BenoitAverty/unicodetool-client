af2f690422743982068d42c1bb43909ed9154c79
Benoit Averty <benoit.averty@gmail.com>
2017-09-10 19:55:33 +0200

    chore(ci): Return deploy original url even when aliasing

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,8 +7,8 @@ deploy_url=$(cd out/ && now -p --static --name unicodetool-client -t $NOW_TOKEN)
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     >&2 echo "Not a pull request. Skipping alias creation"
 else
+    >&2 echo Aliasing deployment with pull request url...
     >&2 now alias $deploy_url pr$TRAVIS_PULL_REQUEST-unicodetool-client.now.sh -t $NOW_TOKEN
-    deploy_url=https://pr$TRAVIS_PULL_REQUEST-unicodetool-client.now.sh
 fi
 
 echo $deploy_url
